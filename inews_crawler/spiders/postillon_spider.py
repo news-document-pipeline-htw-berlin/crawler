e6ac4e5074704bf019d0447af67b2290634c9685
Janis Schanbacher <janis.schanbacher@posteo.de>
2020-12-09 22:19:27 +0100

    Add fixes and documentation for postillon spider.

--- inews_crawler/spiders/postillon_spider.py
+++ inews_crawler/spiders/postillon_spider.py
@@ -21,11 +21,31 @@ testrun_articles = False
 # 'https://www.der-postillon.com/2020/11/agent-smith.html'
 testrun_article_url = False
 
-year_to_crawl = False  # If False or 0, crawl all years
+year_to_crawl = 2020  # If False or 0, crawl all years
 # limits to crawl only articles of the year beginning with the specified month (newer or equal). If False or 0, crawl entire year. Requires year_to_crawl to not be False
 limit_min_month_of_year_to_crawl = False
 
-CURRENT_YEAR = 2020
+AUTHOR_DIC = {
+    "ssi": "Stefan Sichermann",
+    "dan": "Dan Eckert",
+    "pfg": "Peer Gahmert und Phillip Feldhusen",
+    "fed": "Alexander Bayer",
+    "tla": "Tobias Lauterbach",
+    "swo": "Sebastian Wolking",
+    "bep": "Bernhard Pöschla",
+    "jki": "Julia Kiesselbach",
+    "adg": "Daniel Al-Kabbani",
+    "up": "Die UpTicker-Redaktion",
+    "mate": "Mate Tabula",
+    "lor": "Laura Orlik",
+    "vwi": "Valentin Witt",
+    "coe": "Cornelius Oettle",
+    "sha": "Simon Hauschild",
+    "shp": "Selim Han Polat",
+    "ejo": "Ernst Jordan",
+    "kga": "Kike García"
+}
+#  sch, rag, adl, evw, kop, loc/hei, tom
 
 
 class PostillonSpider(scrapy.Spider):
@@ -33,17 +53,17 @@ class PostillonSpider(scrapy.Spider):
     start_url = root
 
     def start_requests(self):
-        """
+        '''
         Generates a request for the URL specified in start_url and defines self.parse as callback function, if testrun_article_url is not defined. 
         Else a request for testrun_article_url is genereated and self.parse_article is defined as callback function.
-        """
+        '''
         if testrun_article_url:
             yield scrapy.Request(testrun_article_url, callback=self.parse_article, cb_kwargs=dict(long_url=testrun_article_url, published_time="13.11.2020"))
         else:
             yield scrapy.Request(self.start_url, callback=self.parse)
 
     def parse(self, response):
-        """ 
+        ''' 
         Scrape archive for articles
         Parameters
         ----------
@@ -51,15 +71,15 @@ class PostillonSpider(scrapy.Spider):
             the PostillonSpider object
         response:
             The response from a scrapy request
-        """
+        '''
         def init_selenium_driver():
-            """
+            '''
             Initialize and return a firefox selenium driver.
 
             Returns
             -------
             A firefox selenium driver
-            """
+            '''
             firefoxOptions = webdriver.FirefoxOptions()
             firefoxOptions.headless = True
             desired_capabilities = firefoxOptions.to_capabilities()
@@ -70,13 +90,13 @@ class PostillonSpider(scrapy.Spider):
             return driver
 
         def get_closed_elements():
-            """
+            '''
             Returns all or some closed year and month elements, depending on the limit definitions.
 
             Returns
             -------
             All or some closed year and month elements, depending on the limit definitions.
-            """
+            '''
             # Get all closed months of year to crawl, that are newer or equal to the limit specified by limit_min_month_of_year_to_crawl
             if limit_min_month_of_year_to_crawl:
                 # get year
@@ -111,24 +131,27 @@ class PostillonSpider(scrapy.Spider):
             return closed_elements
 
         def waitForLoad():
-            """
+            '''
             Wait until at 1 article per year has been loaded. 
             If the current year is being crawled wait until an article of january or limit_min_month_of_year_to_crawl 
             has been loaded (Because the current month of the current year is already loaded on page load).
 
-            """
-            TIMEOUT = 10
+            '''
+            CURRENT_YEAR = datetime.now().year
+            TIMEOUT = 20
             wait = WebDriverWait(driver, TIMEOUT)
             try:
-                xpath = "//a/div/div/div[contains(@class, 'date') and contains(string(), '."
+                # xpath for tag that with class 'date' and content that includes '2020' or '1.2020' or '<limit_min_month_of_year_to_crawl>.2020',
+                # depending on what is to be crawled
+                xpath = "//a/div/div/div[contains(@class, 'date') and contains(string(), '"
                 if year_to_crawl:
-                    # If the current year is crawled wait for an article of the first month to be loaded. 
+                    # If the current year is crawled wait for an article of the first month to be loaded.
                     # This is necessary because the current month is already loaded on page load.
                     if year_to_crawl == CURRENT_YEAR:
                         if limit_min_month_of_year_to_crawl:
                             xpath += str(limit_min_month_of_year_to_crawl) + "."
                         else:
-                            xpath += str(1) + "."
+                            xpath += "1."
 
                     xpath += str(year_to_crawl) + "')]"
                     wait.until(EC.presence_of_element_located(
@@ -138,6 +161,7 @@ class PostillonSpider(scrapy.Spider):
                 else:
                     base_xpath = xpath
                     for i in range(2008, CURRENT_YEAR+1):
+                        # xpath for tag with class 'date' and the content that includes the year i
                         xpath = base_xpath + str(i) + "')]"
                         wait.until(EC.presence_of_element_located(
                             (By.XPATH, xpath)))
@@ -147,20 +171,19 @@ class PostillonSpider(scrapy.Spider):
                     "TimeoutException has been thrown while waiting for articles to load: %s", e)
 
         def click_elements(elements):
-            """"
+            '''"
             Click all elements in elements
 
             Parameters
             ----------
             elements:
                 HTML Elements to be clicked
-            """
+            '''
             for element in elements:
                 try:
                     # element.click() causes Exception: "could not be scrolled into view"
                     driver.execute_script("arguments[0].click();", element)
-                    print("click: " +
-                          element.get_attribute('class').split()[1])
+                    # print("click: " + element.get_attribute('class').split()[1])
 
                 except Exception as e:
                     logging.warning(
@@ -181,13 +204,16 @@ class PostillonSpider(scrapy.Spider):
         # Hand-off between Selenium and Scrapy
         sel = Selector(text=driver.page_source)
 
+        # for all ul tags with class 'month-inner' get all contained li tags and get their direct a-tag children
         articleList = sel.xpath('//ul[@class="month-inner"]//li/a')
 
         articleList = utils.limit_crawl(articleList, testrun_articles)
 
         if articleList:
             for article in articleList:
+                # extract the value of the href attribute from article
                 long_url = article.xpath('./@href').extract()[0]
+                # extract the content of div-tags with class 'date' contained by article 
                 published_time = article.xpath(
                     './/div[@class="date"]/text()').extract()
                 published_time = published_time[0] if len(
@@ -197,7 +223,6 @@ class PostillonSpider(scrapy.Spider):
                     yield scrapy.Request(long_url, callback=self.parse_article,  cb_kwargs=dict(long_url=long_url, published_time=published_time))
 
                 else:
-                    print("url in db")
                     utils.log_event(utils(), self.name,
                                     long_url, 'exists', 'info')
                     logging.info('%s already in db', long_url)
@@ -206,7 +231,7 @@ class PostillonSpider(scrapy.Spider):
         driver.quit()
 
     def parse_article(self, response, long_url, published_time):
-        """
+        '''
         Parse the article contained in response and save the results (and long_url, published_time) in an ArticleItem.
 
         Parameters
@@ -219,27 +244,28 @@ class PostillonSpider(scrapy.Spider):
             Url of the article to be parsed
         published_time:
             Published time of the article to be parsed
-        """
+        '''
 
         utils_obj = utils()
-        print("parsing article: " + long_url)
+        # print("parsing article: " + long_url)
 
         def get_article_text():
-            """
+            '''
             Returns the text of the article
 
             Returns
             -------
             The text of the article
-            """
+            '''
             article_paragraphs = []
+            # extract any p, a or b tag contained in a div with class 'post hentry'
             tags = response.xpath(
                 '//div[@class="post hentry"]//p|a|b').extract()
-
             for tag in tags:
                 line = ""
                 tag_selector = Selector(text=tag)
-                text_list = tag_selector.xpath('//*/text()').extract()
+                # extract the text from all tags contained in tag_selector 
+                text_list = tag_selector.xpath('//text()').extract()
                 for text_part in text_list:
                     line += text_part
                 article_paragraphs.append(line)
@@ -252,8 +278,9 @@ class PostillonSpider(scrapy.Spider):
 
             if not text:
                 # Alternative article layout. Examples https://www.der-postillon.com/2019/11/deutsche-bahn-hack.html , 'https://www.der-postillon.com/2020/02/fehler-stabhochsprung.html'
+                # extract text from all tags contained in a div that an attribute itemprop with value 'article body' or 'description articleBody'
                 text_list = response.xpath(
-                    '//div[@itemprop="articleBody"]//text()').extract()
+                    '//div[@itemprop="articleBody" or @itemprop="description articleBody"]//text()').extract()
 
                 # slicing off after length computation
                 # Slice off everything after "Lesen Sie auch:"
@@ -263,8 +290,8 @@ class PostillonSpider(scrapy.Spider):
 
                 # Slice off credits
                 len_text_list = len(text_list)
-                for i in range(1, 4):
-                    if any(substring in text_list[len_text_list-i] for substring in ['ssi', 'dan', 'pfg', 'fed', 'Foto', '\n']):
+                for i in range(1, min(len_text_list, 4)):
+                    if any(substring in text_list[len_text_list-i] for substring in list(AUTHOR_DIC.keys())):
                         text_list = text_list[:len_text_list-i]
 
                 # Combine text_list
@@ -280,13 +307,13 @@ class PostillonSpider(scrapy.Spider):
             return text
 
         def get_pub_time():
-            """
+            '''
             Returns the published time of the article
 
             Returns
             -------
             The published time of the article
-            """
+            '''
             try:
                 # input: 21.53.09"
                 return datetime.strptime(published_time, '%d.%m.%Y')
@@ -297,7 +324,7 @@ class PostillonSpider(scrapy.Spider):
                 return None
 
         def filter(strings, substring):
-            """
+            '''
             Filter strings by substring
 
             Parameters
@@ -310,22 +337,24 @@ class PostillonSpider(scrapy.Spider):
             Returns
             -------
             String array containing all strings of strings, that include substring 
-            """
+            '''
 
             return [str for str in strings if substring in str]
 
         def get_keywords():
-            """
+            '''
             Returns the keywords of the article
 
             Returns
             -------
             The keywords of the article
-            """
+            '''
 
             # get script that contains the keywords
             keyword_script = filter(response.xpath(
-                './/div/script/text()').extract(), "blogLabels.push")[0]
+                './/div/script/text()').extract(), "blogLabels.push")
+            keyword_script = keyword_script[0] if len(
+                keyword_script) > 0 else ''
             # split to get one string per keyword and sort out strings that do not contain a keyword
             keywords_with_junk = filter(
                 keyword_script.split("blogLabels.push('"),
@@ -339,42 +368,23 @@ class PostillonSpider(scrapy.Spider):
             return keywords
 
         def get_authors():
-            """
+            '''
             Returns the authors of the article
 
             Returns
             -------
             The authors of the article
-            """
+            '''
 
             authors = []
-            author_dict = {
-                "ssi": "Stefan Sichermann",
-                "dan": "Dan Eckert",
-                "pfg": "Peer Gahmert und Phillip Feldhusen",
-                "fed": "Alexander Bayer",
-                "tla": "Tobias Lauterbach",
-                "swo": "Sebastian Wolking",
-                "bep": "Bernhard Pöschla",
-                "jki": "Julia Kiesselbach",
-                "adg": "Daniel Al-Kabbani",
-                "up": "Die UpTicker-Redaktion",
-                "mate": "Mate Tabula",
-                "lor": "Laura Orlik",
-                "vwi": "Valentin Witt",
-                "coe": "Cornelius Oettle",
-                "sha": "Simon Hauschild",
-                "shp": "Selim Han Polat",
-                "ejo": "Ernst Jordan"
-            }
-
+            # extract text from all span-tags contained in a div with attribute itemprop='articleBody'
             potential_credit_strings = response.xpath(
                 '//div[@itemprop="articleBody"]//span/text()').extract()
 
             for potential_credit_string in potential_credit_strings:
-                for author_key in list(author_dict.keys()):
+                for author_key in list(AUTHOR_DIC.keys()):
                     if author_key in potential_credit_string:
-                        authors.append(author_dict[author_key])
+                        authors.append(AUTHOR_DIC[author_key])
 
             if len(authors) == 0:
                 authors.append("Der Postillon")
@@ -387,21 +397,23 @@ class PostillonSpider(scrapy.Spider):
         item['long_url'] = utils.add_host_to_url(utils_obj, long_url, root)
         item['short_url'] = long_url
         item['news_site'] = "postillon"
+        # xpath: value of the content attribute from any meta tag with the attribute property="og:title". (in get_item_string() the first matching meta tag will be used)
         item['title'] = utils.get_item_string(utils_obj, response, 'title', long_url, 'xpath',
                                               ['//meta[@property="og:title"]/@content'], self.name)
         item['authors'] = get_authors()
+        # xpath: value of the content attribute of any meta tag with attribute name='description' or name="twitter:description"
         item['description'] = utils.get_item_string(utils_obj, response, 'description', long_url, 'xpath',
-                                                    ['//meta[@name="description"]/@content'], self.name)
-        item['intro'] = utils.get_item_string(utils_obj, response, 'description', long_url, 'xpath',
-                                              ['//meta[@name="description"]/@content'], self.name)
+                                                    ['//meta[@name="description" or @name="twitter:description"]/@content'], self.name)
+        item['intro'] = item['description']
         item['text'] = get_article_text()
         item['keywords'] = get_keywords()
         item['published_time'] = get_pub_time()
-
+        # xpath: value of the content attribute from any meta tag with the attribute property='og:image'
         image_links = utils.get_item_list(utils_obj, response, 'image_links', long_url, 'xpath',
                                           ['//meta[@property="og:image"]/@content'], self.name)
         item['image_links'] = utils.add_host_to_url_list(
             utils_obj, image_links, root)  # if image_link starts with '/' prepend host
+        # xpath: link fromm any a-tag contained in any div with atribute itemprop='articleBody'
         item['links'] = utils.get_item_list(utils_obj, response, 'links', long_url, 'xpath',
                                             ['.//div[@itemprop="articleBody"]//a/@href'], self.name)
 
@@ -410,7 +422,7 @@ class PostillonSpider(scrapy.Spider):
         #     item['description'], item['intro'], item['text'], item['keywords'], item['keywords'], item['published_time'],
         #     item['image_links'], item['links'])
 
-        # Only save articles without title or text
+        # Do not save articles without title or text
         if item['title'] and item['text']:
             yield item
         else:
